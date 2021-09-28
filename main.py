f1742074ebb35e74bfe17fe57da604beb5c7a97a
Dikshitha0812 <71216869+Dikshitha0812@users.noreply.github.com>
2021-09-28 22:50:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,67 @@
-import pandas as pd
 import csv
 
-df = pd.read_csv('CSV/Final_Star_Data.csv')
+rows = []
 
-del df["index"]
+with open("main.csv", 'r') as f:
+    csvreader = csv.reader(f)
+    for row in csvreader:
+        rows.append(row)
 
-star_name = df["Star_name"]
-distance = df["Distance"]
-mass = df["Mass"]
-radius = df["Radius"]
+headers = rows[0]
+star_data_rows = rows[1:]
 
-new_mass = []
-new_radius = []
+headers[0] = 'row_num'
 
-for i in range(len(mass)):
+for data in star_data_rows:
+    star_mass = data[3]
+    if star_mass == '?':
+        star_data_rows.remove(data)
+        continue
+    star_mass_value = star_mass.split(' ')[0]
+    star_mass_value = star_mass_value.split('–')[0]
     try:
-        var_mass = float(mass[i]) *  1.989e+30
-        var_radius = float(radius[i]) * 6.957e+8
-
-        new_mass.append(var_mass)
-        new_radius.append(var_radius)
-    
+        star_mass_value = int(star_mass_value)
     except:
-        new_mass.append(mass[i])
-        new_radius.append(radius[i])
+        star_mass_value = float(star_mass_value)
+    star_mass_value = float(star_mass_value*1.989e+30)
+    data[3] = star_mass_value
 
-        pass
-
-
-
-gravity = []
-
-for i in range(0, len(distance)):
+for data in star_data_rows:
+    star_radius = data[4]
+    if star_radius == '?':
+        star_data_rows.remove(data)
+        continue
+    star_radius_value = star_radius.split(' ')[0]
+    star_radius_value = star_radius_value.split('-')[0]
+    star_radius_value = star_radius_value.split('–')[0]
     try:
-        var_gravity = (6.674e-11 * float(new_mass[i])) / (float(new_radius[i])**2)
-        gravity.append(var_gravity)
-        
+        star_radius_value = int(star_radius_value)
     except:
-        gravity.append('unknown')
-        pass
+        star_radius_value = float(star_radius_value)
+    star_radius_value = float(star_radius_value*6.957e+8)
+    data[4] = star_radius_value
 
-print(gravity)
+star_gravity = ["Gravity", ]
+star_names = []
+star_masses = []
+star_radii = []
 
-rows = pd.DataFrame(list(zip(star_name, distance,new_mass, new_radius, gravity)), columns=["Star Name", "Distance", "Mass", "Radius","Gravity"])
-rows.to_csv("Star_Data.csv")
+for data in star_data_rows:
+    if data[3] == '?' or data[4] == '?':
+        star_data_rows.remove(data)
+        continue
+    star_names.append(data[1])
+    star_masses.append(data[3])
+    star_radii.append(data[4])
+
+for index, name in enumerate(star_names):
+    gravity = (float(star_masses[index])/(float(star_radii[index])**2))*6.674e-11
+    star_gravity.append(gravity)
+
+for index, data in enumerate(star_data_rows):
+    star_data_rows[index].append(star_gravity[index])
+
+with open("final.csv", 'a+') as f:
+    csvwriter = csv.writer(f)
+    csvwriter.writerow(headers)
+    csvwriter.writerows(star_data_rows)
